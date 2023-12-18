0afcc237bff3ac98c50f84177dc4c70b9f441f3f
杨亚梦 <mz.yyam@gmail.com>
2023-12-18 11:45:36 +0800

    优化时间工具

--- src/common/helpers/date-helper.ts
+++ src/common/helpers/date-helper.ts
@@ -1,9 +1,14 @@
-import { format, formatInTimeZone } from 'date-fns-tz';
+import { format, formatInTimeZone, zonedTimeToUtc } from 'date-fns-tz';
 
-export const formatISO = (date: Date) =>
+export const timeZone = 'Asia/Shanghai';
+
+export const fmtISO = (date: Date, tz?: string) =>
   date
-    ? format(date, "yyyy-MM-dd'T'HH:mm:ssxxx", { timeZone: 'Asia/Shanghai' })
+    ? format(date, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx", { timeZone: tz || timeZone })
     : undefined;
 
 export const fmtBy = (date: Date, fmt: string) =>
-  date ? formatInTimeZone(date, 'Asia/Shanghai', fmt) : undefined;
+  date ? formatInTimeZone(date, timeZone, fmt) : undefined;
+
+export const utc = (date: string, tz?: string) =>
+  date ? zonedTimeToUtc(date, tz || timeZone) : null;
